11a4a69584295976373eb81bcbd97705453850d2
Freddy Guerra <arcobaleno262@gmail.com>
2022-10-23 23:36:00 -0300

    renombar y escribir el main

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-var elemSectionCarrito = document.getElementsByClassName("section-carrito")[0]; //TODO
-class main {
+class Main {
   //AJAX
   async ajax(url, metodo = "get") {
     try {
@@ -10,31 +9,80 @@ class main {
       console.error(err);
     }
   }
-
-  getNombreArchivo(id) {
+  // Property
+  //llama el archivo
+  getNameArch(id) {
     return "views/" + id + ".html";
   }
-
-  marcarLink(id) {
+  //señala el archivo que esta activo
+  markLink(id) {
     const links = document.querySelectorAll("header nav a");
     links.forEach((link) => {
       if (link.id === id) link.classList.add("active");
       else link.classList.remove("active");
     });
   }
-
+  //arranca el respectivo JS de su archivo html
   initJS(id) {
-    if( id === 'alta' ) {
-        initAlta()
+    if (id === "alta") {
+      initAlta();
+    } else if (id === "init") {
+      initInit();
+    } else if (id === "we") {
+      initWe();
+    } else if (id === "contact") {
+      initContact();
     }
-    else if( id === 'inicio' ) {
-        initInicio()
-    }
-    else if( id === 'nosotros' ) {
-        initNosotros()
-    }
-    else if( id === 'contacto' ) {
-        initContacto()
-    }
+  }
+  //Carga las plantillas
+  async ChargerTemplate(id) {
+    let archived = this.getNameArch(id);
+
+    let template = await this.ajax(archived);
+
+    // Carga del código de la vista (HTML) de la platilla
+    let main = document.querySelector("main");
+    main.innerHTML = plantilla;
+
+    // Carga dle código script (JS) de la plantilla
+    this.initJS(id);
+  }
+
+  async ChargerTemplates() {
+    // Carga inicial de la vista deteminada por la url visitada
+    let id = location.hash.slice(1) || "inicio"; // #inicio => slice(1) => inicio
+    this.markLink(id);
+    await this.ChargerTemplate(id);
+
+    // Carga de cada uno de los contenidos según la navegación local
+    const links = document.querySelectorAll("header nav a");
+    //console.log(links)
+
+    links.forEach((link) => {
+      link.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        let id = link.id;
+        //console.log(id)
+        location.hash = id;
+      });
+    });
+
+    window.addEventListener("hashchange", async () => {
+      // console.log('Cambió la URL')
+
+      let id = location.hash.slice(1) || "inicio";
+      this.marcarLink(id);
+      await this.ChargerTemplate(id);
+    });
+  }
+
+  //estable la funcion syncronica que inicializa TODO el main
+  async start() {
+    await this.ChargerTemplates();
+  }
 }
-}
+//llamando a las clase...
+
+const main = new Main();
+main.start();
