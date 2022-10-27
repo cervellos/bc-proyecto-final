5141c6dd1a42a2502f2ad44a8dd84d04e59ad6f2
Freddy Guerra <arcobaleno262@gmail.com>
2022-10-26 23:18:35 -0300

    carrito armado

---
+++ js/services/whishlist.js
@@ -0,0 +1,10 @@
+class WhishlistService {
+  URL_WHISHLIST = "https://633ccbeff2b0e623dc67cf47.mockapi.io/productos";
+
+  async saveListSave(list) {
+    const listSave = await http.post(this.URL_WHISHLIST.list);
+    return listSave;
+  }
+}
+
+const whishlistService = new WhishlistService();
